fe01eb3e4d44245cd037e966a696801f9afd8f2d
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-03-20 11:06:17 +0100

    Fix #21

--- src/IntelliViews.Data/DataContext.cs
+++ src/IntelliViews.Data/DataContext.cs
@@ -43,42 +43,6 @@ namespace IntelliViews.Data
                 .Navigation(t => t.Threads)
                 .AutoInclude();
 
-            // Relationship one-to - one between Thread and Feedback
-            /* builder.Entity<ThreadUser>()
-                .HasOne(t => t.Feedback)
-                .WithOne(f => f.Thread)
-                .HasForeignKey<ThreadUser>(t => t.Id);*/
-
-            /*   // Relationship one-to-many between User and Feedbacks
-               builder.Entity<ApplicationUser>()
-                   .HasMany(u => u.Feedbacks)    
-                   .WithOne(f => f.User)         
-                   .HasForeignKey(f => f.UserId);  
-
-
-               // Relationship one-to-many between User and Threads
-               builder.Entity<ThreadUser>()
-                   .HasOne(t => t.User)
-                   .WithMany(u => u.Threads)
-                   .HasForeignKey(t => t.UsereId);
-
-               //*/
-
-
-
-            // Testing with seed: //TODO: Need to change the UserId..
-          /*  builder.Entity<ThreadUser>().HasData(
-                new ThreadUser() { Id = "1", CreatedAt = DateTime.Now, UserId = "0f058217-784c-4483-ad7d-9d4e01a81933" },
-                new ThreadUser() { Id = "2", CreatedAt = DateTime.Now, UserId = "2450d037-6235-498c-b1e7-ceaf8cafb53a" },
-                new ThreadUser() { Id = "3", CreatedAt = DateTime.Now, UserId = "cdf987f1-81df-4de6-93eb-6182e8f4031a" }
-                );
-
-            builder.Entity<Feedback>().HasData(
-               new Feedback() { Id = "1111", Context = "TestFeedback1", CreatedAt = DateTime.Now, ThreadId = "1", UserId = "0f058217-784c-4483-ad7d-9d4e01a81933", Score = 1 },
-               new Feedback() { Id = "2222", Context = "TestFeedback2", CreatedAt = DateTime.Now, ThreadId = "2", UserId = "2450d037-6235-498c-b1e7-ceaf8cafb53a", Score = 10 },
-               new Feedback() { Id = "3333", Context = "TestFeedback3", CreatedAt = DateTime.Now, ThreadId = "3", UserId = "cdf987f1-81df-4de6-93eb-6182e8f4031a", Score = 9 }
-               );*/
-
         }
 
         public DbSet<ThreadUser> Threads { get; set; }
--- src/IntelliViews.Data/DbEntity.cs
+++ src/IntelliViews.Data/DbEntity.cs
@@ -2,8 +2,6 @@
 {
     public interface DbEntity
     {
-        /*public string Id { get; set; } = Guid.NewGuid().ToString();
-        public DateTime CreatedAt { get; set; } = DateTime.Now;*/
         public string Id { get; set; } 
         public DateTime CreatedAt { get; set; } 
     }
